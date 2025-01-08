82bf7b48c41b68980b265afb8e9daebaed398340
RamadhaniRi <ramadhani9112@gmail.com>
2025-01-08 05:22:18 -0800

    Update and rename Project10-FindMinimumManually.py to Project11-FindMinimumManually.py

--- Project11-FindMinimumManually.py
+++ Project11-FindMinimumManually.py
@@ -3,6 +3,6 @@ def find_minimum(numbers):
     for i in numbers:
         if i < minimum:
             minimum = i
-        return minimum
+    return minimum #Pindahkan return ke luar loop, karena kalau tidak return akan mengembalikan nilai setelah loop pertama
 
 print(find_minimum([6, 4, 3, 8, 9]))
