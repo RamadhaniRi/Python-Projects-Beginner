a53ff017d046c9e2b2a519f0e59d7ea36142719a
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-01 17:50:34 +0700

    new project

---
+++ nested-if.py
@@ -0,0 +1,57 @@
+print("Hello, do you have a moment ?")
+print("I want to ask you a few questions.")
+print()
+print("don't worry becaouse i'm a magician, I will predict your future")
+print()
+name = input("What is your name ?")
+day = input("What day were you born ?")
+
+if day == "Monday":
+    age = int(input("How old are you ?"))
+    if age >= 30:
+        print(name + " You will be a millionaire")
+    else:
+        print(name + " You must focus on your study")
+elif day == "Tuesday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You will be a good father")
+  else:
+      print(name + " You must develop your social skill")
+elif day == "Wednesday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You will find your true love")
+  else:
+      print(name + " Your girlfriend is cheating on you")
+elif day == "Tuesday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You should invest on the stock market")
+  else:
+      print(name + " You are handsome")
+elif day == "Friday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You will be an astronaut")
+  else:
+      print(name + " You will be a good programmer")
+elif day == "Saturday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You will have a ferrari car next year")
+  else:
+      print(name + " You will be a good dancer")
+elif day == "Sunday":
+  age = int(input("How old are you ?"))
+  if age >= 30:
+      print(name + " You will be a good politician")
+  else:
+      print(name + " You will be a good singer")
+else:
+   print("can you speak in english ?")
+      
+        
+
+
+
