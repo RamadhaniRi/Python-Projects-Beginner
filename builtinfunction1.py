06f042599ff0b7fd1cf0704b6361ddc9c1721875
RamadhaniRi <ramadhani9112@gmail.com>
2025-06-03 20:33:02 +0700

    learn built in function

---
+++ builtinfunction1.py
@@ -0,0 +1,21 @@
+# 1. Cetak nama kamu
+print("Nama saya adalah...")
+
+# 2. Tampilkan tipe data dari angka dan string
+print(type(42))
+print(type("Belajar Python"))
+
+# 3. Hitung panjang dari string
+kata = "Pemrograman dani" #spasi juga dihitung
+print(len(kata))
+
+# 4. Bulatkan angka pi ke 3 angka desimal
+print(round(3.14159, 3))
+
+nama = input("Masukkan namamu: ")
+print("Panjang namamu adalah:", len(nama))
+print("Tipe data dari nama:", type(nama))
+
+angka = float(input("Masukkan angka desimal: "))
+print("Angka setelah dibulatkan:", round(angka, 2))
+
